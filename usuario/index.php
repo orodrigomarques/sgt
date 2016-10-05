749593757303e31abd09b00c3177f203f39188d0
Wellsgs <wellsgoncalves@gmail.com>
2016-10-05 20:37:50 -0300

    excluir com pagina gerencia

--- usuario/index.php
+++ usuario/index.php
@@ -51,7 +51,7 @@
 			$limit;   
     
     $usuarios = mysqli_query($conexao, $sql_usuarios);   
-    
+
     
 ?>
 <script type="text/javascript">
@@ -178,7 +178,7 @@
                                                                 <td style="width:40%" class="center">
                                                                     <a href="gerencia.php?id=<?php echo base64_encode($usuario['id_usuario']); ?>&acao=editar" onClick="buscaPessoa('<?php echo($usuario['id_usuario']);?>')" class="btn btn-primary"><i class="icon-trash">&nbsp;&nbsp;Editar</i> </a>
                                                                     <a href="gerencia.php?id=<?php echo base64_encode($usuario['id_usuario']); ?>&acao=visualisar"onClick="buscaPessoa('<?php echo($usuario['id_usuario']);?>')" class="btn btn-success"><i class="icon-trash">&nbsp;&nbsp;Visualizar</i> </a>
-                                                                    <a  onClick="if(confirm('Tem certeza que deseja excluir este registro?')){location.href='excluir.php?id_usuario=<?php echo $usuario['id_usuario']; ?>'}" class="btn btn-danger"> <i class="icon-trash">&nbsp;&nbsp;Excluir</i> </a>
+                                                                    <a  onClick="if(confirm('Tem certeza que deseja excluir este registro?')){location.href='gerencia.php?acao=excluir&id_usuario=<?php echo $usuario['id_usuario']; ?>'}" class="btn btn-danger"> <i class="icon-trash">&nbsp;&nbsp;Excluir</i> </a>
  <!--                                                                   <a onClick="if(confirm('Tem certeza que deseja excluir este usuario?')){location.hrer='gerencia.php?id=<?php echo base64_encode($usuario['id_usuario']); ?>&acao=excluir';}" class="btn btn-danger"> <i class="icon-trash">&nbsp;&nbsp;Excluir</i> </a> -->
                                                                     
                                                                 </td>
@@ -248,29 +248,29 @@
 <script type="text/javascript">!window.jQuery.ui && document.write(unescape('%3Cscript src="assets/js/jqueryui-1.10.3.min.js'))</script>
 -->
 
-<script type='text/javascript' src='assets/js/jquery-1.10.2.min.js'></script> 
-<script type='text/javascript' src='assets/js/jqueryui-1.10.3.min.js'></script> 
-<script type='text/javascript' src='assets/js/bootstrap.min.js'></script> 
-<script type='text/javascript' src='assets/js/enquire.js'></script> 
-<script type='text/javascript' src='assets/js/jquery.cookie.js'></script> 
-<script type='text/javascript' src='assets/js/jquery.touchSwipe.min.js'></script> 
-<script type='text/javascript' src='assets/js/jquery.nicescroll.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/codeprettifier/prettify.js'></script> 
-<script type='text/javascript' src='assets/plugins/easypiechart/jquery.easypiechart.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/sparklines/jquery.sparklines.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/form-toggle/toggle.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/form-wysihtml5/wysihtml5-0.3.0.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/form-wysihtml5/bootstrap-wysihtml5.js'></script> 
-<script type='text/javascript' src='assets/plugins/fullcalendar/fullcalendar.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/form-daterangepicker/daterangepicker.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/form-daterangepicker/moment.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/charts-flot/jquery.flot.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/charts-flot/jquery.flot.resize.min.js'></script> 
-<script type='text/javascript' src='assets/plugins/charts-flot/jquery.flot.orderBars.min.js'></script> 
-<script type='text/javascript' src='assets/demo/demo-index.js'></script> 
-<script type='text/javascript' src='assets/js/placeholdr.js'></script> 
-<script type='text/javascript' src='assets/js/application.js'></script> 
-<script type='text/javascript' src='assets/demo/demo.js'></script> 
+<script type='text/javascript' src='../assets/js/jquery-1.10.2.min.js'></script> 
+<script type='text/javascript' src='../assets/js/jqueryui-1.10.3.min.js'></script> 
+<script type='text/javascript' src='../assets/js/bootstrap.min.js'></script> 
+<script type='text/javascript' src='../assets/js/enquire.js'></script> 
+<script type='text/javascript' src='../assets/js/jquery.cookie.js'></script> 
+<script type='text/javascript' src='../assets/js/jquery.touchSwipe.min.js'></script> 
+<script type='text/javascript' src='../assets/js/jquery.nicescroll.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/codeprettifier/prettify.js'></script> 
+<script type='text/javascript' src='../assets/plugins/easypiechart/jquery.easypiechart.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/sparklines/jquery.sparklines.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/form-toggle/toggle.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/form-wysihtml5/wysihtml5-0.3.0.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/form-wysihtml5/bootstrap-wysihtml5.js'></script> 
+<script type='text/javascript' src='../assets/plugins/fullcalendar/fullcalendar.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/form-daterangepicker/daterangepicker.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/form-daterangepicker/moment.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/charts-flot/jquery.flot.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/charts-flot/jquery.flot.resize.min.js'></script> 
+<script type='text/javascript' src='../assets/plugins/charts-flot/jquery.flot.orderBars.min.js'></script> 
+<script type='text/javascript' src='../assets/demo/demo-index.js'></script> 
+<script type='text/javascript' src='../assets/js/placeholdr.js'></script> 
+<script type='text/javascript' src='../assets/js/application.js'></script> 
+<script type='text/javascript' src='../assets/demo/demo.js'></script> 
 
 
 
