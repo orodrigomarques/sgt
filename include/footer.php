6d933c856e87766995316d92a6a842a695fcd612
rmark3z <r.marqu3s@gmail.com>
2016-10-11 12:36:28 -0300

    update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,10 @@
 <footer role="contentinfo">
         <div class="clearfix">
             <ul class="list-unstyled list-inline">
-                <li>AVANT &copy; 2013</li>
-                <!--li class="pull-right"><a href="javascript:;" id="back-to-top">Top <i class="icon-arrow-up"></i></a></li-->
-                <button class="pull-right btn btn-inverse btn-xs" id="back-to-top" style="margin-top: -1px; text-transform: uppercase;"><i class="icon-arrow-up"></i></button>
+                <li class="pull-left" style="margin-top: -10px"><h5>SGT - Sistema de Gerenciamento de Transportes</h5></li>
+                <!-- <li><img src="assets/img/sgt_icon.png" alt="SGT" class="brand" /></li> -->
+                <li class="pull-right">Desenvolvido por<a href="/contato.php" > <img src="/assets/img/adsix_logo.png"> </i></a></li>
+                <!-- <button class="pull-right btn btn-inverse btn-xs" id="back-to-top" style="margin-top: -1px; text-transform: uppercase;"><i class="icon-arrow-up"></i></button> -->
             </ul>
         </div>
-    </footer>
+</footer>
