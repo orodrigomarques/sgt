a0882963d5eca6e6c954e9c36f8ca41582866508
Shield12 <rubens-cesar@hotmail.com>
2016-10-12 03:58:08 -0300

    ATUALIZACAO VERIFICACAO DE ACESSO
    Impede que o usuario que não seja admistrador ou gerente acesse a pagina

--- usuario/gerencia.php
+++ usuario/gerencia.php
@@ -122,6 +122,10 @@
        
         <?php include '../include/menu.php';?>
  
+        <?php  if($_SESSION['permissao']!= 1 && $_SESSION['permissao']!= 2){
+        echo '<script>alert("Acesso Restrito!'. '\n' .'Você será redirecionado para a tela inicial!");
+                   location.href="../home.php"</script>';
+    }?> 
 <div id="page-content">
     <div id='wrap'>
         <div id="page-heading">
