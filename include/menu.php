a5d491689e5e0130583330e8b5b910757175989f
Shield12 <rubens-cesar@hotmail.com>
2016-10-12 01:46:32 -0300

    Update menu.php
    atualização do menu com o link para associações

--- include/menu.php
+++ include/menu.php
@@ -1,9 +1,13 @@
 <nav id="page-leftbar" role="navigation">
                 <!-- BEGIN SIDEBAR MENU -->
             <ul class="acc-menu" id="sidebar">
-                <li><a href="../home.php"><i class="icon-home"></i> <span>Home</span></a></li>
-                <li><a href="../usuario/index.php"><i class="icon-user"></i> <span>Usuarios</span></a></li>
-                <li><a href="../usuario/index.php"><i class="icon-cog"></i> <span>Cadastros</span></a></li>
+                    <li><a href="../home.php"><i class="icon-home"></i> <span>Home</span></a></li>
+                    <li><a href="../usuario/index.php"><i class="icon-user"></i> <span>Usuarios</span></a></li>
+                    <li><a href="javascript:;"><i class="icon-cog"></i> <span>Cadastros</span> </a>
+                    <ul class="acc-menu">
+                    <li><a href="../associacao/index.php"><i class="icon-th"></i> <span>Associações</span></a></li>
+                    </li>
+                </ul>
                 <li><a href="../contato.php"><i class="icon-envelope"></i> <span>Contato</span></a></li>
 
                 
