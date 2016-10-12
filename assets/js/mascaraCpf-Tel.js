6d526ba25a3073e60718c9c19417ff83a4acaf8b
Shield12 <rubens-cesar@hotmail.com>
2016-10-12 01:19:38 -0300

    Create mascaraCpf-Tel.js
    Mascara para utilizar no telefone e cpf

---
+++ assets/js/mascaraCpf-Tel.js
@@ -0,0 +1,28 @@
+
+    function mascara(o, f) {
+        v_obj = o
+        v_fun = f
+        setTimeout("execmascara()", 1)
+    }
+
+    function execmascara() {
+        v_obj.value = v_fun(v_obj.value)
+    }
+
+    function cpf_mask(v) {
+        v = v.replace(/\D/g, "") //Remove tudo o que não é dígito
+        v = v.replace(/(\d{2})(\d)/, "$1.$2") //Coloca ponto entre o terceiro e o quarto dígitos
+        v = v.replace(/(\d{3})(\d)/, "$1-$2") //Coloca barra após o quinto dígito
+
+        return v
+    }
+
+
+    function tel_mask(v) {
+        v = v.replace(/\D/g, "") //Remove tudo o que não é dígito
+        v = v.replace(/(\d{0})(\d)/, "$1($2") //Coloca parentece antes do primeiro digito
+        v = v.replace(/(\d{2})(\d)/, "$1)$2") //Coloca parentece após o segundo digito
+        v = v.replace(/(\d{4})(\d)/, "$1-$2") //Coloca barra após o quarto digito
+
+        return v
+    }
