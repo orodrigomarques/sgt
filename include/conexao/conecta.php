5cc242b774431de78375fb3bb5de69c4314dc826
weslley <wellsgoncalves@gmail.com>
2016-10-27 00:11:48 -0200

    Conexão feita com PDO

--- include/conexao/conecta.php
+++ include/conexao/conecta.php
@@ -22,4 +22,28 @@ mysqli_select_db($conexao, $dbName);
 // TÍTULO DAS PÁGINAS
 //$titulo = "Painel de controle";
 //--
+
+    function conecta(){
+        //DADOS PARA CONEXAO COM O BANCO DE DADOS LOCAL
+        $dbHost		= "localhost";
+        $dbName		= "sgt"; 
+        $dbUser		= "root";
+        $dbPassword	= "root";
+        //--
+
+        //DADOS PARA CONEXAO COM O BANCO DE DADOS DOMINIO
+        //$dbHost		= "mysql.hostinger.com.br";
+        //$dbName		= "u698970389_sgt"; 
+        //$dbUser		= "u698970389_sgt";
+        //$dbPassword	= "fatecpg";
+        //--
+        try{
+            $conexao = new PDO('mysql:host='.$dbHost.';dbname='.$dbName , $dbUser, $dbPassword);
+        } catch (Exception $ex) {
+            $echo= $ex->getMessage();
+        }
+        return $conexao;
+
+        
+    }
 ?>
