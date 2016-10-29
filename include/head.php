05303b8050283fbec63c9bd79936091134e336d2
Shield12 <rubens-cesar@hotmail.com>
2016-10-28 22:21:03 -0200

    Update head.php
    inclusão do logo na aba da pagina

--- include/head.php
+++ include/head.php
@@ -3,6 +3,9 @@
 <head>
 	<meta charset="utf-8" />
 	<title>SGT</title>
+	 <link rel="icon" 
+      type="image/jpg" 
+      href="../assets/img/sgt_logo.png" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="Avant" />
