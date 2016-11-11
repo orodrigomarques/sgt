75a8361d0d46cb47c69295178e32a068b46ef1f3
Shield12 <rubens-cesar@hotmail.com>
2016-11-11 17:14:09 -0200

    pdo

--- associacao/gerencia.php
+++ associacao/gerencia.php
@@ -1,8 +1,9 @@
 <?php 
     include '../include/head.php';
-    include '../include/funcoes.php';
-    include '../include/conexao/conecta.php';
-    validaAcesso();  
+include '../include/funcoes.php';
+include '../include/conexao/conecta.php';
+$conexao = conecta();
+validaAcesso(); 
         
 
     if(isset($_GET['acao']) && $_GET['acao'] != ''){
@@ -13,29 +14,27 @@
         $id = base64_decode($id);
         
         if($acao == 'excluir'){
-            
-           
+          $deletaAssociacao = $conexao->prepare("DELETE FROM associacao WHERE cd_associacao = :id");
+        $deletaAssociacao->bindValue(":id", $id);
+        $deletaAssociacao->execute();
 
-                $deleta = mysqli_query($conexao,"DELETE FROM associacao WHERE cd_associacao = '$id'");
-
-                if($deleta == ''){
-                echo "<script>alert('Houve um erro ao deletar!');
+        if ($deletaAssociacao->rowCount() == 0) {
+            echo "<script>alert('Houve um erro ao deletar!');
                 location.href=\"index.php\"</script>";
-                }else{
-                   echo "<script>alert('Registro excluido com sucesso!');
+        } else {
+            echo "<script>alert('Registro excluido com sucesso!');
                    location.href=\"index.php\"</script>";
-                }
-                   
-        }        
-                
-        if($acao == 'visualizar' || $acao  == 'editar'){
-            $sql = "SELECT * FROM associacao WHERE cd_associacao = ".$id;
-            $linha = mysqli_query($conexao, $sql);
-            $associacao = mysqli_fetch_assoc($linha) or die(mysql_error());
+        }
+    }
+    if ($acao == 'visualizar' || $acao == 'editar') {
+        $pegaAssociacao = $conexao->prepare("SELECT * FROM associacao WHERE cd_associacao = :id");
+        $pegaAssociacao->bindValue(":id", $id);
+        $pegaAssociacao->execute();
+        $associacao = $pegaAssociacao->fetch(PDO::FETCH_ASSOC);
 
-            $id = $associacao['cd_associacao'];
+        $id = $associacao['cd_associacao'];
             $razao = $associacao['nm_razao_social'];
-            $logradouro = $associacao['nm_logradouro'];
+            
             $local = $associacao['nm_local'];
             $numero = $associacao['ds_numero'];
             $complemento = $associacao['ds_complemento'];
@@ -46,13 +45,13 @@
             $telefone = $associacao['cd_telefone'];
             $email = $associacao['nm_email'];    
             $nm_linha = $associacao['nm_linha'];
-        }
-        
-        if($acao == 'novo' ){
-            
-            $id = 0;
+    }
+
+    if ($acao == 'novo') {
+
+        $id = 0;
             $razao = "";
-            $logradouro = "";
+            
             $local = "";
             $numero = "";
             $complemento ="";
@@ -63,13 +62,13 @@
             $telefone = "";
             $email = "";
             $nm_linha = "";
-        }    
-    }   
-        
-    if(isset($_POST['cd_associacao']) && $_POST['cd_associacao'] != ''){
+    }
+}
+
+if(isset($_POST['cd_associacao']) && $_POST['cd_associacao'] != ''){
             $id = $_POST['cd_associacao'];;
             $razao = $_POST['nm_razao_social'];
-            $logradouro = $_POST['nm_logradouro'];
+           
             $local = $_POST['nm_local'];
             $numero = $_POST['ds_numero'];;
             $complemento = $_POST['ds_complemento'];
@@ -80,33 +79,75 @@
             $telefone = $_POST['cd_telefone'];;
             $email = $_POST['nm_email'];
             $nm_linha = $_POST['nm_linha'];
-        
-        
-        
-        if(empty($id)){
-             
-                $sqlAssociacao = "INSERT INTO associacao (nm_razao_social, nm_logradouro, nm_local, ds_numero,"
-                        . "ds_complemento, cd_cep, nm_bairro, nm_municipio, nm_UF, cd_telefone, nm_email, nm_linha) "
-                                ."VALUES ('".$razao."', '".$logradouro."', '".$local."', ".$numero.", '".$complemento."', ".$cep.", '".$bairro."', '".$municipio."', '".$uf."', '".$telefone."', '".$email."', '".$nm_linha."')"; 
-                echo $sqlAssociacao;
-                $res = mysqli_query($conexao, $sqlAssociacao) or die(mysqli_error($conexao));
-                $retorno='inserido';
-            
-        }else{            
-            $sqlAssociacao = "UPDATE associacao SET nm_razao_social = '".$razao."', nm_logradouro = '".$logradouro."', nm_local = '".$local."', ds_numero = ".$numero.", ds_complemento = '".$complemento."', cd_cep = ".$cep.", nm_bairro = '".$bairro."', nm_municipio = '".$municipio."', nm_UF = '".$uf."', cd_telefone = '".$telefone."', nm_email = '".$email."', nm_linha = '".$nm_linha."' "
-                        . "WHERE cd_associacao = ".$id ; 
-                                
-            $res = mysqli_query($conexao, $sqlAssociacao) or die(mysqli_error());
-            $retorno='alterado';
 
-            
-        }  
-        if($retorno){
-            header('Location: index.php?retorno='.$retorno);
-        }else{
-            header('Location: gerencia.php?id='.base64_encode($id).'&acao=novo&retorno=invalido');
+
+
+    if (empty($id)) {
+
+
+        try {
+            $novaAssociacao = $conexao->prepare("INSERT INTO associacao (nm_razao_social, nm_local, ds_numero, ds_complemento, cd_cep,"
+                    . "nm_bairro, nm_municipio, nm_UF, cd_telefone, nm_email, nm_linha) "
+                    . "VALUES ( :razao, :local, :numero, :complemento, :cep, :bairro, :municipio, :uf, :telefone, :email, :nm_linha )");
+            $novaAssociacao->bindValue(":razao", $razao, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":local", $local, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":numero", $numero);
+            $novaAssociacao->bindValue(":complemento", $complemento, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":cep", $cep);
+            $novaAssociacao->bindValue(":bairro", $bairro, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":municipio", $municipio, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":uf", $uf, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":telefone", $telefone, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":email", $email, PDO::PARAM_STR);
+            $novaAssociacao->bindValue(":nm_linha", $nm_linha, PDO::PARAM_STR);
+
+            $novaAssociacao->execute();
+            // echo $novoUsuario->rowCount();
+            //var_dump($novoUsuario);
+
+
+            $retorno = 'inserido';
+        } catch (Exception $e) {
+            echo $e;
+            exit($e);
         }
-    } 
+    } else {
+        try {
+            $atualizarAssociacao = $conexao->prepare("UPDATE associacao SET nm_razao_social = :razao, nm_local = :local, ds_numero = :numero, ds_complemento = :complemento, cd_cep = :cep, nm_bairro = :bairro, nm_municipio = :municipio, nm_UF = :uf, cd_telefone = :telefone, nm_email = :email, nm_linha = :nm_linha "
+                    . "WHERE cd_associacao = :id");
+            $atualizarAssociacao->bindValue(":razao", $razao, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":local", $local, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":numero", $numero);
+            $atualizarAssociacao->bindValue(":complemento", $complemento, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":cep", $cep);
+            $atualizarAssociacao->bindValue(":bairro", $bairro, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":municipio", $municipio, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":uf", $uf, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":telefone", $telefone, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":email", $email, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":nm_linha", $nm_linha, PDO::PARAM_STR);
+            $atualizarAssociacao->bindValue(":id", $id);
+            $atualizarAssociacao->execute();
+
+//                echo $atualizarUsuario->rowCount();
+//                var_dump($atualizarUsuario);
+//                echo $atualizarUsuario->errorCode();
+//                exit();
+
+            $retorno = 'alterado';
+        } catch (Exception $e) {
+            echo $e;
+            exit();
+        }
+
+    }
+
+   if ($retorno) {
+        header('Location: index.php?retorno=' . $retorno);
+    } else {
+        header('Location: gerencia.php?id=' . base64_encode($id) . '&acao=novo&retorno=invalido');
+    }
+}
 ?>
 <script>alertaSucesso("a", "a", "a")</script>
 
@@ -156,18 +197,7 @@
                     <input name="cd_cep" id="cd_cep" type="text" class="form-control" onblur="pesquisacep(this.value);" value="<?php echo $cep?>" <?php if($acao == 'visualizar'){?>readonly="readonly" <?php };?> required/>
                 </div>
             </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Logradouro</label>
-                <div class="col-sm-4">
-                    <select name="nm_logradouro" id="nm_logradouro" class="form-control" <?php if($acao == 'visualizar'){?>disabled="disabled" <?php };?> required>
-                        <option value=''>-</option>
-                        <option value="AV" <?php echo($logradouro == 'AV') ? 'selected' : '';?>>AV</option>
-                        <option value="RUA" <?php echo($logradouro == 'RUA') ? 'selected' : '';?>>RUA</option>
-                        <option value="ROD." <?php echo($logradouro == 'ROD.') ? 'selected' : '';?>>ROD.</option>
-                        
-                    </select>
-                </div>
-            </div>
+            
             <div class="form-group">
                 <label class="col-sm-2 control-label">Endereço</label>
                 <div class="col-sm-4">
