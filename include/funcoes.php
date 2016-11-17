0206bcf654d9deb684a5f9bc3964835f2cf0416b
Wellsgs <wellsgoncalves@gmail.com>
2016-11-16 23:35:28 -0200

    adicionando função de aulditoria

--- include/funcoes.php
+++ include/funcoes.php
@@ -19,4 +19,27 @@
 
     }
 
+ 
+    function auditoria($acao){
+        
+        include 'conexao/conecta.php';
+        $conexao = conecta();
+                
+        try{
+        
+        $auditoria = $conexao->prepare("INSERT INTO `sgt`.`auditoria` (`id_usuario`, `nm_usuario`, `ds_acao`, `dt_momento`, `nr_ip`) "
+                                         ."VALUES (:id, :usuario, :acao , now(), :ip)");
+        $auditoria->bindValue(":id", $_SESSION['cdUsuario']);
+        $auditoria->bindValue(":usuario", $_SESSION['nomeUsuario']);
+        $auditoria->bindValue(":acao", $acao);
+        $auditoria->bindValue(":ip", $_SERVER['REMOTE_ADDR']);
+        $auditoria->execute();
+        
+        }  catch (Exception $ex){
+            return $ex->getMessage();
+        }
+        
+
+    }
+
 ?>
