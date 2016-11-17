db878402a76aeac0072da84210a989f89c954761
Shield12 <rubens-cesar@hotmail.com>
2016-11-17 14:11:40 -0200

    inclusão da tabela usuario no arquivo

--- shield.sql
+++ shield.sql
@@ -1,3 +1,26 @@
+DROP DATABASE IF EXISTS `sgt`;
+CREATE DATABASE IF NOT EXISTS `sgt` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `sgt`;
+
+
+-- Copiando estrutura para tabela sgt.usuario
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nm_usuario` varchar(50) NOT NULL,
+  `ds_senha` varchar(50) NOT NULL,
+  `ds_ativo` int(11) NOT NULL,
+  `ds_permissao` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela sgt.usuario: ~0 rows (aproximadamente)
+DELETE FROM `usuario`;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` (`id_usuario`, `nm_usuario`, `ds_senha`, `ds_ativo`, `ds_permissao`) VALUES
+	(1, 'wells', '202cb962ac59075b964b07152d234b70', 1, 1),
+	(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 1);
+
 
 CREATE TABLE tipoPessoa(
   cd_tipo_pessoa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
