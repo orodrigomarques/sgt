a10a7caa44a521eae365608903bd515bd01f1512
Shield12 <rubens-cesar@hotmail.com>
2016-11-20 14:56:57 -0200

    Update mascaraCpf-Tel.js

--- assets/js/mascaraCpf-Tel.js
+++ assets/js/mascaraCpf-Tel.js
@@ -1,4 +1,5 @@
-function mascara(o, f) {
+
+    function mascara(o, f) {
         v_obj = o
         v_fun = f
         setTimeout("execmascara()", 1)
@@ -10,7 +11,8 @@ function mascara(o, f) {
 
     function cpf_mask(v) {
         v = v.replace(/\D/g, "") //Remove tudo o que não é dígito
-        v = v.replace(/(\d{2})(\d)/, "$1.$2") //Coloca ponto entre o terceiro e o quarto dígitos
+        v = v.replace(/(\d{3})(\d)/, "$1.$2") //Coloca ponto entre o terceiro e o quarto dígitos
+        v = v.replace(/(\d{3})(\d)/, "$1.$2") //Coloca ponto entre o terceiro e o quarto dígitos
         v = v.replace(/(\d{3})(\d)/, "$1-$2") //Coloca barra após o quinto dígito
 
         return v
@@ -24,4 +26,22 @@ function mascara(o, f) {
         v = v.replace(/(\d{4})(\d)/, "$1-$2") //Coloca barra após o quarto digito
 
         return v
+    }
+    function cel_mask(v) {
+        v = v.replace(/\D/g, "") //Remove tudo o que não é dígito
+        v = v.replace(/(\d{0})(\d)/, "$1($2") //Coloca parentece antes do primeiro digito
+        v = v.replace(/(\d{2})(\d)/, "$1)$2") //Coloca parentece após o segundo digito
+        v = v.replace(/(\d{5})(\d)/, "$1-$2") //Coloca barra após o quarto digito
+
+        return v
+    }
+    
+function mascaraRG(v){
+         v = v.replace(/\D/g, "") //Remove tudo o que não é dígito
+        v = v.replace(/(\d{2})(\d)/, "$1.$2") //Coloca parentece antes do primeiro digito
+        v = v.replace(/(\d{3})(\d)/, "$1.$2") //Coloca parentece após o segundo digito
+        v = v.replace(/(\d{3})(\d)/, "$1-$2") //Coloca barra após o quarto digito
+
+        return v
 }
+
