36bf664986def4cd6bf9ee2eed1adc071c551f12
Shield12 <rubens-cesar@hotmail.com>
2016-11-23 10:30:21 -0200

    Update mascaraCpf-Tel.js
    inclusao de marcara de valor

--- assets/js/mascaraCpf-Tel.js
+++ assets/js/mascaraCpf-Tel.js
@@ -44,4 +44,23 @@ function mascaraRG(v){
 
         return v
 }
-
+function mascaraValor(v){
+   v = v.replace(/\D/g,"");
+ 	v = new String(Number(v));
+        var len = v.length;
+ 	if (1 == len)
+ 	v = v.replace(/(\d)/,"0,0$1");
+ 	else if (2 == len)
+ 	v = v.replace(/(\d)/,"0,$1");
+ 	else if (len > 2 & len < 6) {
+ 	v = v.replace(/(\d{2})$/,',$1');
+ 	}else if (len > 5 & len == 6 ){
+ 	v = v.replace(/(\d{2})$/,',$1');
+        v = v.replace(/(\d{1})(\d)/, "$1.$2") //Coloca barra após o quarto digito
+ 	}else  {
+ 	v = v.replace(/(\d{2})$/,',$1');
+        v = v.replace(/(\d{2})(\d)/, "$1.$2") //Coloca barra após o quarto digito
+ 	}
+ 	return v;
+ 	
+}
