e45f8bf97288148ce03a8648fdc3601ee9465ba3
Shield12 <rubens-cesar@hotmail.com>
2016-11-23 17:01:38 -0200

    Update completar.php

--- completar.php
+++ completar.php
@@ -3,7 +3,7 @@
 
 $q=strtolower ($_GET["q"]);
 
-$sql = "SELECT * FROM usuario WHERE nm_usuario like '%" . $q . "%'";
+$sql = "SELECT * FROM veiculo WHERE cd_placa like '%" . $q . "%'";
 
 $query = mysqli_query($conexao, $sql);// or die ("Erro". mysql_query());
 
@@ -11,7 +11,7 @@ if(mysqli_num_rows($query)>0){
 while($reg=mysqli_fetch_array($query)){
 
 	//if (srtpos(strtolower($reg['nom_lista']),$q !== false){
-		echo $reg["nm_usuario"]."|".$reg["nm_usuario"]."\n";
+		echo $reg["cd_placa"]."|".$reg["cd_placa"]."\n";
 //	}
 }}else{echo "A consulta não retornou nenhum registro";};
 ?>
