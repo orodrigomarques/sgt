d77a2f3e369e475a7d519f1ff7b6ee3a0258da4d
Shield12 <rubens-cesar@hotmail.com>
2016-12-07 11:28:23 -0200

    Update menu.php
    alteração nos icones do menu

--- include/menu.php
+++ include/menu.php
@@ -2,20 +2,21 @@
                 <!-- BEGIN SIDEBAR MENU -->
             <ul class="acc-menu" id="sidebar">
                     <li><a href="../home.php"><i class="icon-home"></i> <span>Home</span></a></li>
-                    <?php if($_SESSION['permissao']== 1){?>
-              <li><a href="../veiculo/index.php"><i class="icon-user"></i> <span>Veiculos</span></a></li>  
+                    <li><a href="../veiculo/index.php"><i class="icon-road"></i> <span>Veiculos</span></a></li>  
+                        <?php if($_SESSION['permissao']== 1){?>
+              
                     <li><a href="../usuario/index.php"><i class="icon-user"></i> <span>Usuarios</span></a></li>               
-                        <li><a href="../pessoa/index.php"><i class="icon-user"></i> <span>Pessoas</span></a></li><?php }; ?>
+                        <li><a href="../pessoa/index.php"><i class="icon-star-empty"></i> <span>Pessoas</span></a></li><?php }; ?>
                    <li><a href="javascript:;"><i class="icon-cog"></i> <span>Cadastros</span> </a>
             <ul class="acc-menu">
-              <li><a href="../vistoria/index.php"><i class="icon-th"></i> <span>Vistorias</span></a></li>
-                <li><a href="../associacao/index.php"><i class="icon-th"></i> <span>Associações</span></a></li>
-                <li><a href="../modalidade/index.php"><i class="icon-th"></i> <span>Modalidades</span></a></li>
-                <li><a href="../processo/index.php"><i class="icon-th"></i> <span>Processos</span></a></li>
-                <li><a href="../recurso/index.php"><i class="icon-th"></i> <span>Recursos</span></a></li>
-              <li><a href="../multa/index.php"><i class="icon-th"></i> <span>Multas</span></a></li>
-              <li><a href="../infracao/index.php"><i class="icon-th"></i> <span>Infrações</span></a></li>
-                <li><a href="../tipopessoa/index.php"><i class="icon-th"></i> <span>Tipo de Pessoa</span></a></li>
+                 <li><a href="../tipopessoa/index.php"><i class="icon-user"></i> <span>Tipo de Pessoa</span></a></li>
+                <li><a href="../associacao/index.php"><i class="icon-link"></i> <span>Associações</span></a></li>
+                <li><a href="../modalidade/index.php"><i class="icon-maxcdn"></i> <span>Modalidades</span></a></li>
+                <li><a href="../processo/index.php"><i class="icon-archive"></i> <span>Processos</span></a></li>
+                <li><a href="../recurso/index.php"><i class="icon-double-angle-left"></i> <span>Recursos</span></a></li>
+              <li><a href="../multa/index.php"><i class="icon-list-ul"></i> <span>Multas</span></a></li>
+              <li><a href="../infracao/index.php"><i class="icon-book"></i> <span>Infrações</span></a></li>
+                             <li><a href="../vistoria/index.php"><i class="icon-check"></i> <span>Vistorias</span></a></li>
 
         </li>
                 </ul>
